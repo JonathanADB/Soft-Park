8abdddf4755d3079c4f60758099d06a984313bf1
Jonathan <jhonnysoy1@gmail.com>
2024-11-07 00:01:25 +0100

    Eliminar archivos innecesarios

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://JonathanADB.github.io/Soft-Park/frontend/', // URL de tu repositorio
+  base: 'https://JonathanADB.github.io/Soft-Park/', // URL de tu repositorio
 
 });
