e86b878e07fc9c923eb40de6180c5ed613012f1a
Jonathan <jhonnysoy1@gmail.com>
2024-11-06 23:25:31 +0100

    modificacion de yaml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base:'https://JonathanADB.github.io/Soft-Park/'
+  base: 'https://JonathanADB.github.io/Soft-Park/', // URL de tu repositorio
+  build: {
+    outDir: 'dist' // Directorio de salida
+  }
 });
