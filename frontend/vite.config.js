a45e6f218eefc32970cf51a81c3c45c8c9d70fbb
Jonathan <jhonnysoy1@gmail.com>
2024-11-06 22:29:04 +0100

    para deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://jonathanadb.github.io/Soft-Park/'
+  base:'https://jonathanadb.github.io/Soft-Park/'
 });
